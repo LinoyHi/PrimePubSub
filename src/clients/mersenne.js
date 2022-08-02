6d13d1ac605c726c9606f80a1592a5ef9adc298e
LinoyHi <linoyhib14@walla.co.il>
2022-08-02 16:24:06 +0300

    functions fixed and run checked

--- src/clients/mersenne.js
+++ src/clients/mersenne.js
@@ -1,4 +1,4 @@
-function mersenne(num,newnum=num-1){   
+function mersenne(num,newnum=num+1){
     if(newnum%2==0){
         if(newnum==2){
             console.log(`this number - ${num} is a Mersenne number`)
--- src/publishers/PrimesGenerator.js
+++ src/publishers/PrimesGenerator.js
@@ -1,27 +1,31 @@
 import MyEvent from "../common/MyEvent.js";
 
-class PrimeGenerator extends MyEvent{
-    constructor(){
+class PrimeGenerator extends MyEvent {
+    constructor() {
         super()
     }
 
-    nextPrime(num){
-        const next= num+1
-        if(((next%2==0||next%3==0||next%5==0||next%7==0)&&(next!=2||next!=3||next!=5||next!=7))&&num!=1){
-            return this.nextPrime(next)
+    isPrime(num) {
+        if(num==1){
+            return true
         }
-        else{
-            return next
-        }
+        else if (num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++)
+            if (num % i == 0) return false;
+        return true;
     }
 
-    startNextPrimeTill(num=1){
-        let i = 1
-        let count= 1
-        while(i<=num){
-            const next = this.nextPrime(i)
-            setTimeout(()=>this.emmit('getPrimes', next),count*1000)
-            i= next
+    startNextPrimeTill(num = 1) {
+        let i = 0
+        let count = 1
+        while (i <= num) {
+            let newNum = i+1
+            while(!this.isPrime(newNum)){
+                newNum++
+            }
+            const next = newNum
+            setTimeout(() => this.emmit('getPrimes', next), count * 1000)
+            i = next
             count++
         }
     }
