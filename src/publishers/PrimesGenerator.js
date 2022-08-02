efddde594c964c1c225243442c19ff863d87eebd
LinoyHi <linoyhib14@walla.co.il>
2022-08-02 16:58:00 +0300

    functions refactored mersenne and isPrime

--- src/publishers/PrimesGenerator.js
+++ src/publishers/PrimesGenerator.js
@@ -6,10 +6,7 @@ class PrimeGenerator extends MyEvent {
     }
 
     isPrime(num) {
-        if(num==1){
-            return true
-        }
-        else if (num < 2) return false;
+        if (num < 2) return false;
         for (let i = 2; i <= Math.sqrt(num); i++)
             if (num % i == 0) return false;
         return true;
